fd4f26e7680010408e2eae6006819168e945d729
MrAlizadeh <mhalizadeh180@gmail.com>
2024-01-18 17:31:59 +0330

    fixing bug in borad print

--- Maze.cpp
+++ Maze.cpp
@@ -195,7 +195,7 @@ void printLevel(int lvl){ // this function prints the borad of Maze game
                 if(j == 0 && i%2 != 0){
                     cout << alphabets[(i+1)/2] << " ";
                 }
-                else cout << " ";                
+                else cout << "  ";                
                 setcolor( 0x07 );  // Initial color for program
                 if(lvl1[i][j] == '@')setcolor( color_choice );
                 cout << lvl1[i][j] <<" ";
